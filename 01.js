b3661a7d11a583728c4a8f790131229a96169594
Ervaldo <82069785+ErvaldoSilva@users.noreply.github.com>
2026-05-22 18:32:41 -0300

    Add files via upload

---
+++ 01.js
@@ -0,0 +1,27 @@
+
+/*let nome ="ERVALDO";
+let idade = 46;
+let altura = 1.68;
+
+console.log("Nome: ", nome);
+console.log("Idade: ", idade);
+console.log("Altura", altura);
+
+let valor; 
+
+console.log("Valor: ", valor, " | Tipo: ", typeof(valor));
+
+let valor = 16;
+
+console.log("Valor: ", valor, " | Tipo: ", typeof(valor)); 
+
+*/
+
+let valor = "Ervaldo";
+
+console.log("Valor: ", valor, " | Tipo: ", typeof(valor));
+
+let valor = "Ervaldo";
+
+console.log("Valor: ", valor, " | Tipo: ", typeof(valor));
+
